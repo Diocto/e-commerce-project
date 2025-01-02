0d77a4d80e9be51dccfc5fe1f066f3051899b300
diocto <duhee74@naver.com>
2025-01-03 00:24:55 +0900

    mock: 인기 상품 관련 mock api 구현

--- src/main/java/kr/hhplus/be/server/interfaces/api/product/ProductController.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/product/ProductController.java
@@ -7,11 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
     @GetMapping("/{product_id}")
     public ResponseEntity<ProductViewResponseBody> getProduct(@PathParam("product_id") Long productId) {
         return ResponseEntity.ok(new ProductViewResponseBody(1L, "productName", 1000L, "description", 10L));
+    }
+
+    @GetMapping("/popular")
+    public ResponseEntity<List<ProductViewResponseBody>> getPopularProduct() {
+        List<ProductViewResponseBody> productViewResponseBodies = List.of(
+                new ProductViewResponseBody(1L, "productName", 1000L, "description", 10L),
+                new ProductViewResponseBody(2L, "productName2", 2000L, "description2", 20L)
+        );
+        return ResponseEntity.ok(productViewResponseBodies);
     }
 }
